5b3f218b7c7ca849a0733eebe1432719c79e61b1
Song HyeonSeok <lmiz2@naver.com>
2018-08-22 00:00:54 +0900

    hashtableinit

---
+++ src/com/dataStructure/SelfHashTable/HashTable.java
@@ -0,0 +1,16 @@
+package com.dataStructure.SelfHashTable;
+
+import java.util.ArrayList;
+
+public class HashTable<T> {
+
+	ArrayList<T> list;
+	public HashTable() {
+		T t = null;
+		list = new ArrayList<T>();
+		if(t instanceof Integer) {
+			
+		}
+	}
+
+}
