8cb0e5855d845a65e39cbb37b8fe33aee8c5b7ca
Song HyeonSeok <lmiz2@naver.com>
2018-08-24 21:21:11 +0900

    hashcode

--- src/com/dataStructure/DataStructureTest.java
+++ src/com/dataStructure/DataStructureTest.java
@@ -1,17 +1,24 @@
 package com.dataStructure;
 
+import com.dataStructure.SelfHashTable.HashTable;
 import com.dataStructure.SelfLinkedList.LInkedList;
 
 public class DataStructureTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		LInkedList list = new LInkedList();
-		list.add("asd");
-		list.add("bsc");
-		list.add("asd1");
-		list.add("bsc2");
-		System.out.println(list.toString());
+//		LInkedList list = new LInkedList();
+//		list.add("asd");
+//		list.add("bsc");
+//		list.add("asd1");
+//		list.add("bsc2");
+//		System.out.println(list.toString());
+		
+		HashTable<Character> hs = new HashTable<>(10);
+		hs.put('c');
+		hs.put('a');
+		hs.put('z');
+		hs.put('f');
 	}
 
 }
--- src/com/dataStructure/SelfHashTable/HashTable.java
+++ src/com/dataStructure/SelfHashTable/HashTable.java
@@ -2,15 +2,56 @@ package com.dataStructure.SelfHashTable;
 
 import java.util.ArrayList;
 
-public class HashTable<T> {
+public class HashTable<Character> {
 
-	ArrayList<T> list;
-	public HashTable() {
-		T t = null;
-		list = new ArrayList<T>();
-		if(t instanceof Integer) {
-			
+	ArrayList[] list;
+	public HashTable(int size) {
+		list = new ArrayList[size];
+		for(int i = 0; i < size ; i ++) {
+			list[i] = new ArrayList<Character>();
 		}
 	}
-
+	
+	public void put(char in) {
+		SelfCharacter input = new SelfCharacter(in);
+		list[input.hashCode()%list.length].add(in);
+		
+		for(int i = 0; i < list.length; i++) {
+			if(list[i].isEmpty()) {
+				System.out.println(i + " : [ ]");
+			}else {
+				System.out.print(i+" : ");
+				for(Object item :list[i]) {
+					if(list[i].isEmpty()) {
+						System.out.println("[ ]");
+					}else {
+						System.out.print("["+(Character)item+"] ");
+					}
+					
+				}
+				System.out.println();
+			}
+		}
+		System.out.println();
+	}
+	
+	class SelfCharacter{
+		char i;
+		SelfCharacter(char c) {
+			// TODO Auto-generated constructor stub
+			i = c;
+		}
+		
+		@Override
+		public int hashCode() {
+			// TODO Auto-generated method stub
+			int a = i;
+			int b = a *10;
+			int c = a *100;
+			a = a * 31 ;
+			a = (b*31) + a ;
+			a = (c*31) + a ;
+			return  a;
+		}
+	}
 }
