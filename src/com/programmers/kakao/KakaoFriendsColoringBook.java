a628b6d4a4bdb226da88b6a05e5d25a15ed1d8da
Song HyeonSeok <lmiz2@naver.com>
2018-08-25 16:41:27 +0900

    init..

--- src/com/programmers/kakao/KakaoFriendsColoringBook.java
+++ src/com/programmers/kakao/KakaoFriendsColoringBook.java
@@ -4,7 +4,9 @@ public class KakaoFriendsColoringBook {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[][] pic = {{1,1,1,0},{1,2,2,0},{1,0,0,1},{0,0,0,1},{0,0,0,3},{0,0,0,3}};
+		Solution s = new Solution();
+		s.solution(6, 4, pic);
 	}
 
 }
@@ -14,9 +16,20 @@ class Solution {
 	      int numberOfArea = 0;
 	      int maxSizeOfOneArea = 0;
 	      
+	      print(m,n,picture);
+	      
 	      int[] answer = new int[2];
 	      answer[0] = numberOfArea;
 	      answer[1] = maxSizeOfOneArea;
 	      return answer;
+	  }
+	  
+	  public void print(int m, int n ,int[][] pct) {
+		  for(int i = 0; i < m; i++) {
+			  for(int j = 0; j < n; j++) {
+				  System.out.print(" "+pct[i][j]+" ");
+			  }
+			  System.out.println();
+		  }
 	  }
 }
