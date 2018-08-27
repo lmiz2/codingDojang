2aee91a95cad19e46d150388e5145c4d59e527e3
lmiz2 <lmiz2@naver.com>
2018-08-27 13:48:18 +0900

    0827

--- src/com/backjun/KIO/Q9663.java
+++ src/com/backjun/KIO/Q9663.java
@@ -1,163 +1,317 @@
 package com.backjun.KIO;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class Q9663 {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while(true) {
-			String in = br.readLine();
-			if(in.equals("exit")) {
-				break;
-			}
-			ComNQ3 cp = new ComNQ3();
-			cp.compute(Integer.parseInt(in));
-		}
-	}
-}
-
-class ComputerNQueen{
-	int totalWayCount = 0;
-	List<int[]> Dtiles;
+	int totalCnt = 0;
 	
-	public void start(int size) {
-		int deps = 0;
-		Dtiles = new ArrayList<int[]>();
-		for(int i = 0; i < size; i++) {//x좌표
-			for(int j = 0; j < size ; j++) {//y좌표
-				nQueen(i,j,size,deps+1);
-			}
-		}
-		System.out.println(totalWayCount);
-	}
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		ComNQ4 c = new ComNQ4();
+//		c.compute(Integer.parseInt(br.readLine()));
+//		
+//
+//	}
 	
-	public void nQueen(int x, int y, int range, int deps) {
+	
+	static int path[] = new int[16];
+	static int n;
+	static int ans;
+	
+	public static void main(String[] args) {
 		
-		if(deps==range) {
-			totalWayCount++;
-			return;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		for(int i=0; i<n; i++){
+			path[0] = i;
+			nQueen(i,0);
 		}
-		
-		for(int rx = 0; rx < range ; rx++) {
-			for(int ry = 0; ry < range ; ry++) {
-				int dX = Math.abs(x - rx);
-				int dY = Math.abs(y - ry);//차
-				if(dX > 0 && dY > 0 && dX != dY) {
-					//현재 퀸의 Range 밖 == 놓을 수 있는 tile
-				}
-			}
-		}
-		
+		System.out.println(ans);
 	}
+	private static void nQueen(int x, int y) {
+		for(int i=0;i<y;i++){
+	        if(path[i]==x || Math.abs(x-path[i])==y-i)	        	
+	        	 return;
+	    }
+	    if(y==n-1){
+	        ans++;
+	        return;
+	    }
+	    for(int i=0;i<n;i++){
+	        path[y+1]=i;
+	        nQueen(i,y+1);
+	    }
+	}
+
 	
 }
-
-class ComNQ2{
-	
-	int totalCount = 0;
-	int size = 0;
-	public void compute(int size) {
-		this.size = size;
-		queen(0,0,0,0,1);
-	}
-	
-	public void queen(int x, int y, int w, int z, int deps) {
-		System.out.println("queen"+deps);
-		if(deps == size) {
-			totalCount++;
-			return;
-		}
-		int luRd = size + (x-y);
-		int ldRu = x+y+1;
-		for(int i = 0 ; i < size; i++) {
-			for(int j = 0; j < size; j++) {
-				if( x != i && y != j && w != luRd && z != ldRu) {
-					queen(i,j,luRd,ldRu,deps+1);
-				}
-			}
-		}
-		if(deps == 1) {
-			System.out.println(totalCount);
-		}
-	}
-}
-
-class ComNQ3{
-	
-	int totalCount = 0;
-	int size = 0;
-	public void compute(int size) {
-		this.size = size;
-		queen();
-	}
-	
-	public void queen(int ...x) {
-		if(x.length == 4) {
-			totalCount++;
-			return;
-		}
-//		int luRd = size + (x-y);
-//		int ldRu = x+y+1;
-//		for(int i = 0 ; i < size; i++) {
-//			for(int j = 0; j < size; j++) {
-//				if( x != i && y != j && w != luRd && z != ldRu) {
-//					queen(i,j,luRd,ldRu,deps+1);
+//
+//class ComNQ4{
+////	double stTime = 0;	
+//	int totalCount = 0;
+//	int size = 0;
+//	int deps = 0;
+//	HashSet<Integer> qY;
+//	HashSet<Integer> qld;
+//	HashSet<Integer> qlu;
+//	
+//	public void newQueen(int x, int y) {
+//		qY.add(y);
+//		qld.add(x+y);
+//		qlu.add(x-y);
+//	}
+//	
+//	public void popQueen(int x, int y) {
+//		qY.remove(y);
+//		qld.remove(x+y);
+//		qlu.remove(x-y);
+//	}
+//	
+//	public void compute(int size) {
+//		qY = new HashSet<Integer>();
+//		qld = new HashSet<Integer>();
+//		qlu = new HashSet<Integer>();
+//		this.size = size;
+////		long start = System.currentTimeMillis();
+//		for(int y = 0; y < this.size ; y++) {
+//			newQueen(0,y);
+//			queen(0,deps+1);
+//			popQueen(0, y);
+//		}
+////		long end = System.currentTimeMillis();
+////		stTime += (end - start)/1000.0;
+//		System.out.println(totalCount);
+////		System.out.println("total exe Time : "+stTime);
+//	}
+//	
+//	public void queen(int xt, int deps) {
+//		if(deps == size) {
+//			totalCount++;
+//			return;
+//		}
+//		else {
+//			
+//			for(int x = xt+1; x < size; x++) {
+//				for(int y = 0; y < size ; y++) {
+//					if(!qY.contains(y)) {
+//						if(!qld.contains(x+y) && !qlu.contains(x-y)) {
+//							newQueen(x, y);
+//							queen(x,deps+1);
+//							popQueen(x, y);
+//						}
+//					}
 //				}
 //			}
 //		}
-		switch(x.length) {
-		case 0 :
-			for(int i = 0; i < size ; i++) {// 가로
-				queen(i);
-			}
-			break;
-		case 1 :
-			for(int i = 0; i < size ; i++) {// 세로
-				queen(x[0],i);
-			}
-			break;
-		case 2 :
-			int luRd = size + (x[0]-x[1]);
-			for(int i = 0; i < size*2-1 ; i++) {//대각선1
-				if(i == luRd) {
-					queen(x[0],x[1],i);
-				}
-			}
-			break;
-		case 3 :
-			for(int i = 0; i < size*2-1 ; i++) {//대각선2
-				int ldRu = x[0]+x[1]+1;
-				if(i == ldRu) {
-					queen(x[0],x[1],x[2],i);
-				}
-			}
-			break;
-		default : 
-			break;
-		}
-		
-		if(x.length == 0) {
-			System.out.println(totalCount);
-		}
-	}
-}
+//	}
+//	
+//
+//} //시간초과 코드
 
 
-class ComNQ4{
-	
-	int totalCount = 0;
-	int size = 0;
-	public void compute(int size) {
-		this.size = size;
-		queen();
-	}
-	
-	public void queen(int x) {
-		
-	}
-}
+
+
+//
+//package com.backjun.KIO;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.HashSet;
+//import java.util.Stack;
+//
+//public class Q9663 {
+//	int totalCnt = 0;
+//	
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		ComNQ4 c = new ComNQ4();
+//		c.compute(Integer.parseInt(br.readLine()));
+//		
+//
+//	}
+//	
+//}
+//
+//class ComNQ4{
+//	double stTime = 0;
+//	double ioTime = 0;
+//	double ldt = 0;
+//	double lut = 0;
+//	double yt = 0;
+//	
+//	int totalCount = 0;
+//	int size = 0;
+//	int deps = 0;
+//	HashSet<Integer> qY;
+//	HashSet<Integer> qld;
+//	HashSet<Integer> qlu;
+//	Stack<Queen> ql ;
+//	
+//	public void newQueen(int x, int y) {
+//		qY.add(y);
+//		qld.add(x+y);
+//		qlu.add(x-y);
+//	}
+//	
+//	public void popQueen(int x, int y) {
+//		qY.remove(y);
+//		qld.remove(x+y);
+//		qlu.remove(x-y);
+//	}
+//	
+//	public void compute(int size) {
+//		this.ql = new Stack<Queen>();
+//		qY = new HashSet<Integer>();
+//		qld = new HashSet<Integer>();
+//		qlu = new HashSet<Integer>();
+//		this.size = size;
+//		long start = System.currentTimeMillis();
+//		for(int x = 0; x < this.size; x++) {
+//			for(int y = 0; y < this.size ; y++) {
+//				newQueen(x,y);
+//				queen(x,deps+1);
+//				ql.pop();
+//
+//			}
+//		}
+//		long end = System.currentTimeMillis();
+//		stTime += (end - start)/1000.0;
+//		System.out.println(totalCount);
+//		System.out.println("total exe Time : "+stTime);
+//		System.out.println("total ld Time : "+ldt);
+//		System.out.println("total lu Time : "+lut);
+//		System.out.println("total yt Time : "+yt);
+//	}
+//	
+//	public void queen(int xt, int deps) {
+//		if(deps == size) {
+//			totalCount++;
+//			return;
+//		}
+//		else {
+//			
+//			for(int x = xt+1; x < size; x++) {
+//					for(int y = 0; y < size ; y++) {
+//						if(!isInY(y)) {
+//								if(!isInld(x,y) && !isInlu(x,y)) {
+//									ql.push(new Queen(x, y));
+//									queen(x,deps+1);
+//									ql.pop();
+//								}
+//						}
+//					}
+//			}
+//		}
+//	}
+//	
+//	
+//	public boolean isInY(int y) {
+//		long start = System.currentTimeMillis();
+//		for (Queen tmpq: ql) {
+//			if(tmpq.getLocationY() == y) {
+//
+//				long end = System.currentTimeMillis();
+//				yt += (end - start)/1000.0;
+//				return true;
+//				
+//			}
+//		}
+//
+//		long end = System.currentTimeMillis();
+//		yt += (end - start)/1000.0;
+//		return false;
+//		
+//	}
+//	
+//	public boolean isInld(int x, int y) {
+//		long start = System.currentTimeMillis();
+//		int ld = x-y;
+//		for (Queen tmpq: ql) {
+//			if(tmpq.getLeftdownToRightup() == ld) {long end = System.currentTimeMillis();
+//			ldt += (end - start)/1000.0;
+//				return true;
+//			}
+//		}
+//		long end = System.currentTimeMillis();
+//		ldt += (end - start)/1000.0;
+//		return false;
+//	}
+//	
+//	public boolean isInlu(int x, int y) {
+//		long start = System.currentTimeMillis();
+//		int lu = x+y;
+//		for (Queen tmpq: ql) {
+//			if(tmpq.getLeftupToRightdown() == lu) {
+//				long end = System.currentTimeMillis();
+//				lut += (end - start)/1000.0;
+//				return true;
+//			}
+//		}
+//		long end = System.currentTimeMillis();
+//		lut += (end - start)/1000.0;
+//		return false;
+//	}
+//	
+//	public int[] getAbsXs() {
+//		return null;
+//			
+//		}
+//	
+//	public int[] getAbsYs() {
+//		return null;
+//		
+//	}
+//}
+//
+//class Queen{
+//	int locationX = 0;
+//	int locationY = 0;
+//	int leftupToRightdown = 0;
+//	int leftdownToRightup = 0;
+//	
+//	Queen(int x, int y){
+//		locationX = x;
+//		locationY = y;
+//		leftupToRightdown = x+y;
+//		leftdownToRightup = x-y;
+//	}
+//	
+//	public int getLeftupToRightdown() {
+//		return leftupToRightdown;
+//	}
+//
+//	public void setLeftupToRightdown(int leftupToRightdown) {
+//		this.leftupToRightdown = leftupToRightdown;
+//	}
+//
+//	public int getLeftdownToRightup() {
+//		return leftdownToRightup;
+//	}
+//
+//	public void setLeftdownToRightup(int leftdownToRightup) {
+//		this.leftdownToRightup = leftdownToRightup;
+//	}
+//
+//	public int getLocationX() {
+//		return locationX;
+//	}
+//	public void setLocationX(int locationX) {
+//		this.locationX = locationX;
+//	}
+//	public int getLocationY() {
+//		return locationY;
+//	}
+//	public void setLocationY(int locationY) {
+//		this.locationY = locationY;
+//	}
+//	
+//	@Override
+//	public String toString() {
+//		return "Queen X:"+locationX+" Y:"+locationY+" ld:"+leftdownToRightup +" lu:"+leftupToRightdown;
+//	}
+//} //시간초과 코드
+//
+//
+
--- src/com/programmers/kakao/KakaoFriendsColoringBook.java
+++ src/com/programmers/kakao/KakaoFriendsColoringBook.java
@@ -1,33 +1,76 @@
 package com.programmers.kakao;
 
+import java.util.ArrayList;
+
 public class KakaoFriendsColoringBook {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[][] pic = {{1,1,1,0},{1,2,2,0},{1,0,0,1},{0,0,0,1},{0,0,0,3},{0,0,0,3}};
+//		int[][] pic = {{0,1,1,0},{1,2,2,0},{1,0,0,1},{0,5,0,1},{0,6,0,3},{0,0,0,3},{2147483647,2147483647,0,2147483647},{2147483647,2147483647,2147483647,2147483647}};
+		int[][] pict = {{0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0},{0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0}
+						, {0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0},{0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0}
+						, {0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0}
+						, {0,1,1,1,1,2,1,1,1,1,2,1,1,1,1,0},{0,1,1,1,2,1,2,1,1,2,1,2,1,1,1,0}
+						, {0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},{0,1,3,3,3,1,1,1,1,1,1,3,3,3,1,0}
+						, {0,1,1,1,1,1,2,1,1,2,1,1,1,1,1,0},{0,0,1,1,1,1,1,2,2,1,1,1,1,1,0,0}
+						, {0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0},{0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0}};
 		Solution s = new Solution();
-		s.solution(6, 4, pic);
+		s.solution(13, 16, pict);
 	}
 
 }
 
 class Solution {
-	  public int[] solution(int m, int n, int[][] picture) {
+	int oneCnt = 0;
+	int areaCnt = 0;
+	int largestSize = 0;
+	public int[] solution(int m, int n, int[][] picture) {
 	      int numberOfArea = 0;
 	      int maxSizeOfOneArea = 0;
-	      
-	      print(m,n,picture);
+//	      print(m,n,picture);
+	      for(int i = 0; i < m; i++) {
+	    	  for(int j = 0 ; j < n ; j ++) {
+	    		  if(picture[i][j] != 0) {
+		    		  deps(picture, i, j, picture[i][j]);
+				      areaCnt++;  
+		    	      if(oneCnt > largestSize) {
+		    	    	  largestSize = oneCnt;
+		    	      }
+		    	      oneCnt = 0;
+	    		  }
+	    	  }		      
+	      }
 	      
 	      int[] answer = new int[2];
+	      numberOfArea = areaCnt;
+	      maxSizeOfOneArea = largestSize;
 	      answer[0] = numberOfArea;
 	      answer[1] = maxSizeOfOneArea;
+	      System.out.println(answer[0]+", "+answer[1]);
 	      return answer;
+	  }
+	  
+	  public void deps(int[][] picture, int x, int y, int color) {
+		  try {
+			  if(picture[x][y] != color) {
+				  return;
+			  }else {
+				  picture[x][y] = 0;
+				  oneCnt++;
+				  deps(picture, x, y+1, color);
+				  deps(picture, x+1, y, color);
+				  deps(picture, x, y-1, color);
+				  deps(picture, x-1, y, color);
+			  }
+		  }catch(ArrayIndexOutOfBoundsException e) {
+			  return;
+		  }
 	  }
 	  
 	  public void print(int m, int n ,int[][] pct) {
 		  for(int i = 0; i < m; i++) {
 			  for(int j = 0; j < n; j++) {
-				  System.out.print(" "+pct[i][j]+" ");
+				  System.out.print(""+(pct[i][j] == 1 ? "@" : pct[i][j] == 2 ? "#": pct[i][j] == 3? "*": " ")+"");
 			  }
 			  System.out.println();
 		  }
