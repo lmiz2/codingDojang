7db38724a6d678400a4f02faed47dbc1a172f284
Song HyeonSeok <lmiz2@naver.com>
2018-10-13 00:41:24 +0900

    Update src/com/backjun/graph/q4963/Q4963.java

--- src/com/backjun/graph/q4963/Q4963.java
+++ src/com/backjun/graph/q4963/Q4963.java
@@ -11,6 +11,7 @@ public class Q4963 {
 	static boolean isWnH = true;
 	static int lineCounter = 0;
 	static int colCounter = 0;
+	static int numberOfIsland = 0;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -38,19 +39,40 @@ public class Q4963 {
 			colCounter = 0;
 			lineCounter++;
 			if(geoMap[0].length == lineCounter) {
-				
-				solution();
+				for(int w = 0 ; w < geoMap.length; w++) {
+					for(int h = 0; h < geoMap[0].length; h++) {
+						solution(w, h, 0);
+					}
+				}
+				System.out.println(numberOfIsland);
+				numberOfIsland = 0;
 				isWnH = true;
 				lineCounter = 0;				
 			}
 		}
 	}
 	
-	public static void solution() {
-		for(int w = 0 ; w < geoMap.length; w++) {
-			for(int h = 0; h < geoMap[0].length; h++) {
-				
+	public static void solution(int w, int h, int deps) {
+		try {
+			if(geoMap[w][h] == 1) {
+				if(deps == 0 ) {
+					numberOfIsland++;
+				}
+				geoMap[w][h] = 0;
+				solution(w-1,h-1,deps+1);
+				solution(w-1,h,deps+1);
+				solution(w-1,h+1,deps+1);
+				solution(w,h+1,deps+1);
+				solution(w+1,h+1,deps+1);
+				solution(w+1,h,deps+1);
+				solution(w+1,h-1,deps+1);
+				solution(w,h-1,deps+1);
+			}else {
+				return;
 			}
+		}catch(ArrayIndexOutOfBoundsException e) {
+			return;
 		}
+		
 	}
 }
