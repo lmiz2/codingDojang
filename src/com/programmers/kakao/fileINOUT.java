0404d0b7f1e5df92e944196474a7edf6f698c7e7
Song HyeonSeok <lmiz2@naver.com>
2018-11-04 14:38:19 +0900

    1104

---
+++ src/com/programmers/kakao/fileINOUT.java
@@ -0,0 +1,15 @@
+package com.programmers.kakao;
+
+import java.io.File;
+import java.io.InputStreamReader;
+
+public class fileINOUT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File fe = new File("C:\\WorkShop\\log.txt");
+		
+		
+	}
+
+}
--- src/com/sortAlgorithm/efficientSort/MergeSort.java
+++ src/com/sortAlgorithm/efficientSort/MergeSort.java
@@ -1,10 +1,41 @@
 package com.sortAlgorithm.efficientSort;
 
 public class MergeSort {
-
+	int[] unsortedArr = {6,5,3,1,8,7,2,4,3};
+	int[] sortedArr ;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MergeSort ms = new MergeSort();
+		ms.sortedArr = new int[ms.unsortedArr.length];
+		
+	}
+	
+	public void merging(int[] arr, int left, int right) {
+		int middle ;
+		
+		if(left<right) {
+			middle = left+right/2;
+			merging(arr, left, middle);
+			merging(arr, middle+1, right);
+			sorting(arr, left, middle, right);
+		}
+	}
+	
+	public void sorting(int[] arr, int left, int middle, int right) {
+		int i, j, k, w;
+		i = left;
+		j = middle+1;
+		k = left;
+		
+		while(i<=middle && j<= right) {
+			if(arr[i] <= arr[j]) {
+				sortedArr[k++] = arr[i++];
+			}else {
+				sortedArr[k++] = arr[j++];
+			}
+		}
 	}
 
 }
+
+
