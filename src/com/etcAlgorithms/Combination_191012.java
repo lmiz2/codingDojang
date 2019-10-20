f6ac67a180650829a93bdd9c7a112ec0e9f341d0
Song HyeonSeok <lmiz2@naver.com>
2019-10-20 11:25:45 +0900

    Update Combination_191012.java

--- src/com/etcAlgorithms/Combination_191012.java
+++ src/com/etcAlgorithms/Combination_191012.java
@@ -6,8 +6,8 @@ public class Combination_191012 {
 	static int cnt = 0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
-		int r = 9;
+		int[] arr = {1,2,3,4,5,6,7,8,9};
+		int r = 6;
 		Combination_191012 c = new Combination_191012();
 		c.comb(arr, arr.length, r, 0, 0, new int[r]);
 		System.out.println(cnt);
@@ -18,9 +18,8 @@ public class Combination_191012 {
 			System.out.println(Arrays.toString(rsArr));
 			return ;
 		}else if (nIdx == n) return;
-		
-		
 		rsArr[rsIdx] = nArr[nIdx];
+//		comb(nArr,n,r-1,nIdx,rsIdx+1,rsArr);//중복 가능한경우
 		comb(nArr,n,r-1,nIdx+1,rsIdx+1,rsArr);
 		comb(nArr,n,r,nIdx+1,rsIdx,rsArr);
 	}
